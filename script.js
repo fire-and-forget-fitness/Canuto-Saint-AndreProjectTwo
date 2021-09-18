a9f110ea8f5f36779d24cc0e4b4246addb596d6e
brcanuto <brian.r.canuto@gmail.com>
2021-09-18 15:10:35 -0400

    Figured out linking submit to API call :sparkle:

--- script.js
+++ script.js
@@ -6,13 +6,79 @@
 
 //The final option is then drawn from the array and printed to the screen via SR accessible text, and an accompanying image.
 
-    fetch('https://wger.de/api/v2/exercise/?results.category/')
-    .then( (response) => {
-    return response.json();
-    })
-    .then( (jsonResult) => {
-    const exercise = jsonResult.results[0].name;
-    console.log(exercise)
-    const paragraphEl = document.querySelector('p.nextDeparture')
-    paragraphEl.innerHTML = `Work on your ${exercise}.`
-    });
+
+
+
+// Idea: User input provides value for ID for Muscle group and eqipment. Based off that value we get the API link Use the code below as an example
+
+// const endpointUrl = new URL('https://api.datamuse.com/words%27)
+
+//   console.log(endpointUrl)
+
+//   endpointUrl.search = new URLSearchParams({
+//     ml: "sandwich"
+//   })
+
+//   fetch(endpointUrl)
+//   .then( response =>  response.json()
+//   )
+//   .then((jsonResult) => {
+//     console.log(jsonResult)
+//   });
+
+
+// endpointUrl.search = new URLSearchParams({
+//     muscles: bodyGroup.value,
+//     equipment: equipGroup.value
+// });
+
+// fetch (endpointUrl)
+//   .then ( response => response.json())
+//   .then((jsonResult) => {
+//       console.log(jsonResult)
+// });
+
+let userInput1 = undefined;
+let userInput2 = undefined;
+
+const form = document.getElementById("userInput");
+
+const bodyGroup = document.getElementById("bodyGroup");
+const equipGroup = document.getElementById("equipGroup");
+
+bodyGroup.addEventListener('change', (event) => {
+  let bodyGroup = form.elements['bodyGroup'];
+  userInput1 = bodyGroup.value;
+
+  console.log(userInput1);
+})
+
+equipGroup.addEventListener('change', (event) => {
+  let equipGroup = form.elements['equipGroup'];
+  userInput2 = equipGroup.value;
+
+  console.log(userInput2);
+})
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log(userInput1, userInput2)
+
+  const endpointUrl = new URL(`https://wger.de/api/v2/exercise/?muscles=${userInput1}&equipment=${userInput2}`)
+
+  console.log(endpointUrl)
+})
+
+
+
+    // fetch('https://wger.de/api/v2/exercise/?results.category/')
+    // .then( (response) => {
+    // return response.json();
+    // })
+    // .then( (jsonResult) => {
+    // const exercise = jsonResult.results[0].name;
+    // console.log(exercise)
+    // const paragraphEl = document.querySelector('p.nextDeparture')
+    // paragraphEl.innerHTML = `Work on your ${exercise}.`
+    // });
