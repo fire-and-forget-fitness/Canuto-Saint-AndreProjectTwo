6e2e53f49555526e5a51c994dd4167e9a0671310
brcanuto <brian.r.canuto@gmail.com>
2021-09-26 15:37:41 -0400

    cleaned up pseudocode and javascript

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 //This is a fitness app. The app will take two user inputs. The inputs will dictate an exercise. The first input will target a muscle group. The second input involves your workout space.
 
-//FIRST INPUT: The user will choose a body area they wish to exercise, and this will create an array for related exercises from the API.
-
-//SECOND INPUT: The user will then choose a second option, these options involve the physical space available to the user, and will draw an optoin from the array created by the first input.
-
-//The final option is then drawn from the array and printed to the screen via SR accessible text, and an accompanying image.
-
-// Idea: User input provides value for ID for Muscle group and eqipment. Based off that value we get the API link Use the code below as an example
+// Build Namespacing and API Request
+  // Make API Call to get exercise names and descriptions
+// Create event listeners in order to get the value of the exercise and equipment
+// Display exercises and description on page based on user input
+  // Use math.floor and math.random in order to get a random number of the index
+  // Use random number of index in order to get exercise name and description
+  // Append selected exercise name and description on page
+ 
+// Bonus/Stretch:
+  // Achieved: Add another language as the API has some other languages available
+  // Not achieved: Add a second API call in order to display pictures
 
 
 // Build Our API Request
 const app = {};
 
- // Make API call to get Name of Exercise
+ // Make API call to get exercise name and exercise description
 app.getExercises = () => {
   const endpointUrl = new URL(`https://wger.de/api/v2/exercise`)
   endpointUrl.search = new URLSearchParams({
-    // limit: 99,
-    // offset: 0,
     language: app.langForm,
     category: app.bodyForm,
     equipment: app.equipForm
-  }); 
+  });   
   fetch (endpointUrl)
     .then ( response => {
       return response.json()})
     .then((jsonResult) => {
       document.querySelector('#exercise').innerHTML = '';
       app.displayExercises(jsonResult.results);
-  });
-};
+  });    
+};  
 
 //  Display Exercise on Page
 
 app.displayExercises = function(exerciseName) {
 
-// If exerciseImage === undefined, run/call API again; else run displayImage
-
   // Selects a random exercise name from the array of the object
   const exerciseId = Math.floor(Math.random() * exerciseName.length);
   const getRandomExerciseName = function() {
-    const filteredName = exerciseName[exerciseId].name
-    return filteredName 
+    const filteredName = exerciseName[exerciseId].name;
+    return filteredName;
     }
   const getRandomExerciseDesc = function() {
-    const filteredDesc = exerciseName[exerciseId].description
-    return filteredDesc 
+    const filteredDesc = exerciseName[exerciseId].description;
+    return filteredDesc;
     }
   
-  // Appends chosen exercise on page
+  // Appends chosen exercise on page  
   const name = document.createElement("h2");
   name.innerText = getRandomExerciseName();
   const desc = document.createElement("p");
@@ -59,13 +59,7 @@ app.displayExercises = function(exerciseName) {
   exercise.appendChild(name);
   exercise.appendChild(desc);
   document.querySelector("#exercise").appendChild(exercise);
-
-  // console.log(exerciseName[exerciseId].images[0])
-  // console.log(exerciseName[exerciseId].images[0].exercise_base)
-  console.log(exerciseName);
-  console.log(exerciseId)
-
-}
+}  
 
 // Event Listeners for Option
 
@@ -75,15 +69,16 @@ app.setUpEventListeners = function () {
     app.bodyForm = document.getElementById("bodyGroup").value;
     app.equipForm = document.getElementById("equipGroup").value;
     app.langForm = document.getElementById("langGroup").value;
-    console.log(app.bodyForm, app.equipForm, app.langForm)
-    app.getExercises(this.value)
-  })
-}
+    app.getExercises(this.value);
+  })  
+}  
 
 
 
 app.init = () => {
-  app.setUpEventListeners()
-};
+  app.setUpEventListeners();
+};  
 
 app.init();
+
+
